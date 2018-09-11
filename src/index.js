6b6a89d6b8d3a2465c7ef771c09d90b72a666e4d
ton9q <kuchma_anton@mail.ru>
2018-09-12 01:33:00 +0300

    addiction of logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  var money = {};
+  var buffer;
+  const obj = {"H" : 50, "Q" : 25, "D" : 10, "N" : 5, "P" : 1};
+
+  if (currency <= 0) 
+    return money;
+  if (currency > 10000) 
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+      
+  for (let o in obj){
+    buffer = Math.floor(currency / obj[o]);
+      
+    if (buffer) {
+      money[o] = buffer;
+      currency -= money[o] * obj[o];
+    }
+  }  
+  
+  return money;
+};
